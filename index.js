16e41f0e1e72aef9c4d559f044103d877eebea16
TwilightLogic <twilightlogic@gmail.com>
2023-04-25 22:14:24 +0800

    Connect the data with firebase

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import {
+  getDatabase,
+  ref,
+  push,
+} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+
 const appSettings = {
-  databaseUrl:
+  databaseURL:
     "https://cute-cart-default-rtdb.asia-southeast1.firebasedatabase.app/",
 };
+
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+const moviesInDB = ref(database, "movies");
 
 const inputFieldEl = document.getElementById("input-field");
 const addButtonEl = document.getElementById("add-button");
@@ -9,5 +20,7 @@ const addButtonEl = document.getElementById("add-button");
 addButtonEl.addEventListener("click", function () {
   let inputValue = inputFieldEl.value;
 
-  console.log(inputValue);
+  push(moviesInDB, inputValue);
+
+  console.log(`${inputValue} added to database!`);
 });
