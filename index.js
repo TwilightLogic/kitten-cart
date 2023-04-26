e239fbd9dbb4542afc774475d11e2a8d10e8ce7f
TwilightLogic <twilightlogic@gmail.com>
2023-04-26 09:27:00 +0800

    Append a new <li> with text content inputValue to the shopping-list <ul>

--- index.js
+++ index.js
@@ -17,11 +17,16 @@ const shoppingListInDB = ref(database, "shoppingList");
 
 const inputFieldEl = document.getElementById("input-field");
 const addButtonEl = document.getElementById("add-button");
+const shoppingList = document.getElementById("shopping-list");
 
+// Add-to-cart event handler
 addButtonEl.addEventListener("click", function () {
   let inputValue = inputFieldEl.value;
 
-  push(shoppingListInDB, inputValue);
+  // 1. push `inputValue` into data base
+  // push(shoppingListInDB, inputValue);
+  // 2. Insert the value as a `<li>` into `<ul>`
+  shoppingList.insertAdjacentHTML("beforeend", `<li>${inputValue}</li>`);
 
   console.log(inputValue);
 });
